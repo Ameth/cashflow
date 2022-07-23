a0899f4ba33cedadfa691fa580dc2a56c3645f48
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-07-23 10:36:26 -0500

    initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <img
+      @click="reload"
+      src="@/assets/cashflow-icon.svg"
+      alt="logo"
+      title="Inicio"
+    />
+    <h1><span>Cash</span>Flow</h1>
+    <span></span>
+  </header>
+</template>
+
+<script setup>
+const reload = () => {
+  location.reload();
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+}
+img,
+span {
+  width: 48px;
+}
+
+img {
+  cursor: pointer;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
